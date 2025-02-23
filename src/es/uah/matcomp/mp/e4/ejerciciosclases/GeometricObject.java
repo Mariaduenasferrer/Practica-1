17c745e5fb9bcfca2b46fa5b1bc817a77f457ae5
Guillermo <guillermo.escobarp@edu.uah.es>
2025-02-23 19:09:54 +0100

    Aquí están subidos mi parte del d

---
+++ src/es/uah/matcomp/mp/e4/ejerciciosclases/GeometricObject.java
@@ -0,0 +1,6 @@
+package es.uah.matcomp.mp.e4.ejerciciosclases;
+
+interface GeometricObject {
+    double getArea();
+    double getPerimeter();
+}
---
+++ src/es/uah/matcomp/mp/e4/ejerciciosclases/Resizable.java
@@ -0,0 +1,5 @@
+package es.uah.matcomp.mp.e4.ejerciciosclases;
+
+interface Resizable {
+    void resize(int percent);
+}

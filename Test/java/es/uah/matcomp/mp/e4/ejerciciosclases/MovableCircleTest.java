ece01a8b85ce288137d37f60f54753d669af405e
Joseluis6735 <josel.cano@edu.uah.es>
2025-02-26 09:42:16 +0100

    Cambios realizados

---
+++ Test/java/es/uah/matcomp/mp/e4/ejerciciosclases/MovableCircleTest.java
@@ -0,0 +1,31 @@
+package es.uah.matcomp.mp.e4.ejerciciosclases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MovableCircleTest {
+
+    @Test
+    void moveUp() {
+    }
+
+    @Test
+    void moveDown() {
+    }
+
+    @Test
+    void moveLeft() {
+    }
+
+    @Test
+    void moveRight() {
+    }
+
+    @Test
+    void resetPosition() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ Test/java/es/uah/matcomp/mp/e4/ejerciciosclases/MovableTest.java
@@ -0,0 +1,23 @@
+package es.uah.matcomp.mp.e4.ejerciciosclases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MovableTest {
+
+    @Test
+    void moveUp() {
+    }
+
+    @Test
+    void moveDown() {
+    }
+
+    @Test
+    void moveLeft() {
+    }
+
+    @Test
+    void moveRight() {
+    }
+}
